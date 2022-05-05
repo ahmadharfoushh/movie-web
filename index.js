2d6123ae4f28eac0f024a3e561e2f12762cd3805
ahmadharfoushh <79727444+ahmadharfoushh@users.noreply.github.com>
2022-05-05 22:22:23 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -19,4 +19,125 @@ $(document).ready(function() {
 
     } , 5000)
 
+    const trending = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://advanced-movie-search.p.rapidapi.com/discover/movie?with_genres=28&page=1",
+        "method": "GET",
+        "headers": {
+            "X-RapidAPI-Host": "advanced-movie-search.p.rapidapi.com",
+            "X-RapidAPI-Key": "583c3ffa31msh5717b01149e07bcp15048djsn77af18ed695e"
+        }
+    };
+
+    $.ajax(trending).done(function (response) {
+        for(var i = 0; i < 10; i++) {
+            $(".trending_wrapper").append(`
+                <div class="movie_card">
+                    <img class="movie_image" src="${ response['results'][i]["poster_path"] }" alt="" width="250">
+                </div>
+            `)
+        }
+    });
+
+    const comedy = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://advanced-movie-search.p.rapidapi.com/discover/movie?with_genres=878&page=1",
+        "method": "GET",
+        "headers": {
+            "X-RapidAPI-Host": "advanced-movie-search.p.rapidapi.com",
+            "X-RapidAPI-Key": "583c3ffa31msh5717b01149e07bcp15048djsn77af18ed695e"
+        }
+    };
+
+    setTimeout(function() {
+
+        $.ajax(comedy).done(function (response) {
+            for(var i = 0; i < 10; i++) {
+                $(".top_ten_wrapper").append(`
+                    <div class="movie_card">
+                        <img class="movie_image" src="${ response['results'][i]["poster_path"] }" alt="" width="250">
+                    </div>
+                `)
+            }
+        });
+
+    }, 5000)
+
+    const family = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://advanced-movie-search.p.rapidapi.com/discover/movie?with_genres=10751&page=1",
+        "method": "GET",
+        "headers": {
+            "X-RapidAPI-Host": "advanced-movie-search.p.rapidapi.com",
+            "X-RapidAPI-Key": "583c3ffa31msh5717b01149e07bcp15048djsn77af18ed695e"
+        }
+    };
+
+    setTimeout(function() {
+
+        $.ajax(family).done(function (response) {
+            for(var i = 0; i < 10; i++) {
+                $(".family_wrapper").append(`
+                    <div class="movie_card">
+                        <img class="movie_image" src="${ response['results'][i]["poster_path"] }" alt="" width="250">
+                    </div>
+                `)
+            }
+        });
+
+    }, 10000)
+
+    const romantic = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://advanced-movie-search.p.rapidapi.com/discover/movie?with_genres=10749&page=1",
+        "method": "GET",
+        "headers": {
+            "X-RapidAPI-Host": "advanced-movie-search.p.rapidapi.com",
+            "X-RapidAPI-Key": "583c3ffa31msh5717b01149e07bcp15048djsn77af18ed695e"
+        }
+    };
+
+    setTimeout(function() {
+
+        $.ajax(romantic).done(function (response) {
+            for(var i = 0; i < 10; i++) {
+                $(".romantic_wrapper").append(`
+                    <div class="movie_card">
+                        <img class="movie_image" src="${ response['results'][i]["poster_path"] }" alt="" width="250">
+                    </div>
+                `)
+            }
+        });
+
+    }, 15000)
+
+    const thriller = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://advanced-movie-search.p.rapidapi.com/discover/movie?with_genres=37&page=1",
+        "method": "GET",
+        "headers": {
+            "X-RapidAPI-Host": "advanced-movie-search.p.rapidapi.com",
+            "X-RapidAPI-Key": "583c3ffa31msh5717b01149e07bcp15048djsn77af18ed695e"
+        }
+    };
+
+    setTimeout(function() {
+
+        $.ajax(thriller).done(function (response) {
+            for(var i = 0; i < 10; i++) {
+                $(".thriller_wrapper").append(`
+                    <div class="movie_card">
+                        <img class="movie_image" src="${ response['results'][i]["poster_path"] }" alt="" width="250">
+                    </div>
+                `)
+            }
+        });
+
+    }, 20000)
+
 })
